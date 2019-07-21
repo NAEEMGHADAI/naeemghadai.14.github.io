d558a70456e30fb92beef451b734c56f3d28a577
NAEEMGHADAI <naeemghadai007.com@gmail.com>
2019-07-21 18:55:09 +0530

    publishing my first website
    I AM HAPPY

---
+++ naeem.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+    function slideshow(){
+  var x = document.getElementById('check-class');
+  if(x.stlye.display==="none"){
+        x.style.display = "block";
+    }else{
+        x.style.display = "none";
+    }
+}
